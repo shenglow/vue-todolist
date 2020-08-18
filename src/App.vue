f57535f23a9693d340c3076666b8d14dff1420a4
shenglow <shenglow1993@gmail.com>
2020-08-19 00:31:57 +0800

    [20200819]first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Todo></Todo>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <h1>todos</h1>
+        <input type="text" autofocus v-model="newTodo" @keyup.enter="addTodo"/>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Todo",
+    data: function() {
+        return {
+            newTodo: ''
+        }
+    },
+    methods: {
+        addTodo: function() {
+            console.log(this.newTodo);
+        }
+    }
+}
+</script>
+
+<style scoped>
+section {
+    width: 100%;
+    max-width: 500px;
+    margin: auto;
+    text-align:center;
+}
+
+h1 {
+    color: red;
+}
+</style>
