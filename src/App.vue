90d040eaaec5de64cf528c9331c0bd2082018876
shenglow <shenglow1993@gmail.com>
2020-08-20 00:21:11 +0800

    [20200820] adjust textbox style

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div id="app">
-    <Todo></Todo>
-  </div>
+    <div id="app">
+        <Todo></Todo>
+    </div>
 </template>
 
 <script>
 import Todo from './components/Todo.vue';
+import './scss/style.scss';
 
 export default {
-  name: 'App',
-  components: {
-    Todo
-  }
+    name: 'App',
+    components: {
+        Todo
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+    background: #f5f5f5;
+}
+
+h1 {
+    margin: 0.5rem 0;
+}
 </style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -4,7 +4,14 @@
             <h1>todos</h1>
         </header>
         <section class="main">
-            <input type="text" autofocus v-model="newTodo" @keyup.enter="addTodo" placeholder="What needs to be done?"/>
+            <input
+                type="text"
+                autofocus
+                class="new-todo"
+                v-model="newTodo"
+                @keyup.enter="addTodo"
+                placeholder="What needs to be done?"
+            />
         </section>
     </section>
 </template>
@@ -14,24 +21,44 @@ export default {
     name: "Todo",
     data: function() {
         return {
+            todos: [],
             newTodo: ''
         }
     },
     methods: {
         addTodo: function() {
-            alert(this.newTodo);
+            this.todos.push(this.newTodo);
+            this.newTodo = "";
+            console.log(this.todos);
         }
     }
 }
 </script>
 
-<style>
-.todoapp h1 {
-    width: 100%;
-    font-size: 100px;
-    font-weight: 100;
-    text-align: center;
-    color: rgba(175, 47, 47, 0.15);
+<style lang="scss">
+.todoapp {
+    .header h1 {
+        width: 100%;
+        font-size: 100px;
+        font-weight: 100;
+        text-align: center;
+        color: rgba(175, 47, 47, 0.15);
+    }
+
+    .main {
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+
+        .new-todo {
+            padding: 16px;
+            width: 100%;
+            border: none;
+            box-sizing: border-box;
+
+            &:focus {
+                outline: none;
+            }
+        }
+    }
 }
 section {
     width: 100%;
